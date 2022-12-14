7b30e57cb977c684f21ed38df2007a4b7c586555
chenmingsan <sam80719@gmail.com>
2022-12-14 14:05:33 +0800

    Blocking Channels

--- main.go
+++ main.go
@@ -12,19 +12,24 @@ func main() {
 		"https://www.facebook.com/",
 		"https://www.google.com/webhp?hl=zh-TW",
 	}
-	for _, link := range links {
-		checkLink(link)
-	}
 
+	c := make(chan string) // 建立字串類型的channels
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+	fmt.Println(<-c)
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 
 	if err != nil {
 		fmt.Println(link, "connection failure")
+		c <- "connection failure"
 		return
 	}
 
 	fmt.Println(link, "connection success")
+	c <- "connection success"
 }
